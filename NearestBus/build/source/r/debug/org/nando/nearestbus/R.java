d59eb60864ac9169ae7bfa0f18d3506811076219
nandihno <fernando.deleon@gmail.com>
2013-09-04 23:47:20 +1000

    fix UI style change name of the app using proper maps certificate

--- NearestBus/build/source/r/debug/org/nando/nearestbus/R.java
+++ NearestBus/build/source/r/debug/org/nando/nearestbus/R.java
@@ -152,34 +152,58 @@ containing a value of this type.
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int blue_button=0x7f020000;
-        public static final int common_signin_btn_icon_dark=0x7f020001;
-        public static final int common_signin_btn_icon_disabled_dark=0x7f020002;
-        public static final int common_signin_btn_icon_disabled_focus_dark=0x7f020003;
-        public static final int common_signin_btn_icon_disabled_focus_light=0x7f020004;
-        public static final int common_signin_btn_icon_disabled_light=0x7f020005;
-        public static final int common_signin_btn_icon_focus_dark=0x7f020006;
-        public static final int common_signin_btn_icon_focus_light=0x7f020007;
-        public static final int common_signin_btn_icon_light=0x7f020008;
-        public static final int common_signin_btn_icon_normal_dark=0x7f020009;
-        public static final int common_signin_btn_icon_normal_light=0x7f02000a;
-        public static final int common_signin_btn_icon_pressed_dark=0x7f02000b;
-        public static final int common_signin_btn_icon_pressed_light=0x7f02000c;
-        public static final int common_signin_btn_text_dark=0x7f02000d;
-        public static final int common_signin_btn_text_disabled_dark=0x7f02000e;
-        public static final int common_signin_btn_text_disabled_focus_dark=0x7f02000f;
-        public static final int common_signin_btn_text_disabled_focus_light=0x7f020010;
-        public static final int common_signin_btn_text_disabled_light=0x7f020011;
-        public static final int common_signin_btn_text_focus_dark=0x7f020012;
-        public static final int common_signin_btn_text_focus_light=0x7f020013;
-        public static final int common_signin_btn_text_light=0x7f020014;
-        public static final int common_signin_btn_text_normal_dark=0x7f020015;
-        public static final int common_signin_btn_text_normal_light=0x7f020016;
-        public static final int common_signin_btn_text_pressed_dark=0x7f020017;
-        public static final int common_signin_btn_text_pressed_light=0x7f020018;
-        public static final int green_button=0x7f020019;
-        public static final int ic_launcher=0x7f02001a;
-        public static final int orange_button=0x7f02001b;
+        public static final int bg_header=0x7f020000;
+        public static final int blue_button=0x7f020001;
+        public static final int blue_gradient=0x7f020002;
+        public static final int btn_default_disabled_focused_holo_dark=0x7f020003;
+        public static final int btn_default_disabled_holo_dark=0x7f020004;
+        public static final int btn_default_focused_holo_dark=0x7f020005;
+        public static final int btn_default_holo_dark=0x7f020006;
+        public static final int btn_default_normal_holo_dark=0x7f020007;
+        public static final int btn_default_pressed_holo_dark=0x7f020008;
+        public static final int common_signin_btn_icon_dark=0x7f020009;
+        public static final int common_signin_btn_icon_disabled_dark=0x7f02000a;
+        public static final int common_signin_btn_icon_disabled_focus_dark=0x7f02000b;
+        public static final int common_signin_btn_icon_disabled_focus_light=0x7f02000c;
+        public static final int common_signin_btn_icon_disabled_light=0x7f02000d;
+        public static final int common_signin_btn_icon_focus_dark=0x7f02000e;
+        public static final int common_signin_btn_icon_focus_light=0x7f02000f;
+        public static final int common_signin_btn_icon_light=0x7f020010;
+        public static final int common_signin_btn_icon_normal_dark=0x7f020011;
+        public static final int common_signin_btn_icon_normal_light=0x7f020012;
+        public static final int common_signin_btn_icon_pressed_dark=0x7f020013;
+        public static final int common_signin_btn_icon_pressed_light=0x7f020014;
+        public static final int common_signin_btn_text_dark=0x7f020015;
+        public static final int common_signin_btn_text_disabled_dark=0x7f020016;
+        public static final int common_signin_btn_text_disabled_focus_dark=0x7f020017;
+        public static final int common_signin_btn_text_disabled_focus_light=0x7f020018;
+        public static final int common_signin_btn_text_disabled_light=0x7f020019;
+        public static final int common_signin_btn_text_focus_dark=0x7f02001a;
+        public static final int common_signin_btn_text_focus_light=0x7f02001b;
+        public static final int common_signin_btn_text_light=0x7f02001c;
+        public static final int common_signin_btn_text_normal_dark=0x7f02001d;
+        public static final int common_signin_btn_text_normal_light=0x7f02001e;
+        public static final int common_signin_btn_text_pressed_dark=0x7f02001f;
+        public static final int common_signin_btn_text_pressed_light=0x7f020020;
+        public static final int edit_text_holo_dark=0x7f020021;
+        public static final int green_button=0x7f020022;
+        public static final int ic_launcher=0x7f020023;
+        public static final int orange_button=0x7f020024;
+        public static final int orange_gradient=0x7f020025;
+        public static final int switch_bg_disabled_holo_dark=0x7f020026;
+        public static final int switch_bg_focused_holo_dark=0x7f020027;
+        public static final int switch_bg_holo_dark=0x7f020028;
+        public static final int switch_inner_holo_dark=0x7f020029;
+        public static final int switch_thumb_activated_holo_dark=0x7f02002a;
+        public static final int switch_thumb_disabled_holo_dark=0x7f02002b;
+        public static final int switch_thumb_holo_dark=0x7f02002c;
+        public static final int switch_thumb_pressed_holo_dark=0x7f02002d;
+        public static final int switch_track_holo_dark=0x7f02002e;
+        public static final int textfield_activated_holo_dark=0x7f02002f;
+        public static final int textfield_default_holo_dark=0x7f020030;
+        public static final int textfield_disabled_focused_holo_dark=0x7f020031;
+        public static final int textfield_disabled_holo_dark=0x7f020032;
+        public static final int textfield_focused_holo_dark=0x7f020033;
     }
     public static final class id {
         public static final int action_settings=0x7f050027;
@@ -342,7 +366,10 @@ containing a value of this type.
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080003;
-        public static final int button_text=0x7f080004;
+        public static final int ButtonAppTheme=0x7f080004;
+        public static final int EditTextAppTheme=0x7f080005;
+        public static final int ImageButtonAppTheme=0x7f080006;
+        public static final int button_text=0x7f080007;
     }
     public static final class styleable {
         /**  From: file:/Users/fernandoMac/developer/java/workspace/NearestBusProject/NearestBus/build/exploded-bundles/ComGoogleAndroidGmsPlayServices3136.aar/res/values/maps_attrs.xml 
